680652a0a531489c49696e612b4546c595117dcc
Mohammad Mehedi Hasan <mohammadmehedihasan84@gmail.com>
2024-12-27 21:09:20 +0600

    project deploy

--- src/firebase/Firebase.config.js
+++ src/firebase/Firebase.config.js
@@ -1,11 +1,11 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey:import.meta.env.VITE_apiKey,
-  authDomain:import.meta.env.VITE_authDomain,
-  projectId:import.meta.env.VITE_projectId,
-  storageBucket:import.meta.env.VITE_storageBucket,
-  messagingSenderId:import.meta.env.VITE_messagingSenderId,
-  appId:import.meta.env.VITE_appId
+  apiKey:"AIzaSyDTw-gkmCv9iJXy7c1Ld8xx1jUWg7DLxy8",
+  authDomain:"react-new-app-1b208.firebaseapp.com",
+  projectId:"react-new-app-1b208",
+  storageBucket: "react-new-app-1b208.firebasestorage.app",
+  messagingSenderId:"979586424942",
+  appId:"1:979586424942:web:d7f3daa0ce8663aba6067f"
 };
 export const app = initializeApp(firebaseConfig);
